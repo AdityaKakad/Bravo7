4d36e8f5b451b53c0b9ac5caaf1a652ced333a32
unknown <abhahema@usc.edu>
2021-03-06 20:23:58 -0800

    Audio toggle

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    private static BackgroundMusic background;
+
+    void Awake()
+    {
+        if (background == null)
+        {
+            background = this;
+            DontDestroyOnLoad(background);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+
+
+}
