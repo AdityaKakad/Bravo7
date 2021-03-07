1b0060e27248e068a13571d891078487acf8b127
stuadmin <stuadmin@itslvlmba20-25.local>
2021-03-06 21:06:22 -0800

    Adding the highscore logic

--- Assets/Scripts/GameOverScore.cs
+++ Assets/Scripts/GameOverScore.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class GameOverScore : MonoBehaviour
 {
     public Text finalScoreText;
+    public Text highScoreText;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,5 +23,11 @@ public class GameOverScore : MonoBehaviour
     {
         int score = PlayerPrefs.GetInt("CurrentScore", 0);
         finalScoreText.text = "Final Score: " + score + "!";
+        int highScore = PlayerPrefs.GetInt("HighScore", 0);
+        if(highScore <= score) {
+            highScore = score; 
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+        highScoreText.text = "High Score: " + highScore + "!";
     }
 }
