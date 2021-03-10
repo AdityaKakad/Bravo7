f6ef236a1bba11de37cd3e59e6625024257aedb3
RushabhK02 <rushabhpkapadia@gmail.com>
2021-03-09 16:46:59 -0800

    Revert Asset change

---
+++ Assets/Input/ArrowKeysDetector.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+
+public class ArrowKeysDetector : MonoBehaviour, IInputDetector
+{
+    public InputDirection? DetectInputDirection()
+    {
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+            return InputDirection.Top;
+        else if (Input.GetKeyUp(KeyCode.DownArrow))
+            return InputDirection.Bottom;
+        else if (Input.GetKeyUp(KeyCode.RightArrow))
+            return InputDirection.Right;
+        else if (Input.GetKeyUp(KeyCode.LeftArrow))
+            return InputDirection.Left;
+        else if (Input.GetKeyUp(KeyCode.Space))
+            return InputDirection.Jump;
+        else
+            return null;
+    }
+}
