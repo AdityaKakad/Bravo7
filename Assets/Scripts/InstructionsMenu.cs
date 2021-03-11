302aef09868fed61487481c78a0a76235f6e8416
RushabhK02 <rushabhpkapadia@gmail.com>
2021-03-11 14:40:33 -0800

    Added game title

---
+++ Assets/Scripts/InstructionsMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InstructionsMenu : MonoBehaviour
+{
+    public void BackToMain()
+    {
+        //Instructions.gameObject.SetActive(true);
+        SceneManager.LoadScene("Menu");
+    }
+}
