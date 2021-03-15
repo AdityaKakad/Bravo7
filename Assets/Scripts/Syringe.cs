40335a90e21e440f5de6e99fdcb650219bbdc6de
RushabhK02 <rushabhpkapadia@gmail.com>
2021-03-14 17:01:00 -0700

    Added more assets

--- Assets/Scripts/Syringe.cs
+++ Assets/Scripts/Syringe.cs
@@ -30,6 +30,6 @@ public class Syringe : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        transform.Rotate(0, 0, turnSpeed * Time.deltaTime);
+        transform.Rotate(turnSpeed * Time.deltaTime, 0, 0);
     }
 }
