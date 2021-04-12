9a6f6d3905198a21359c10349d5f589a40b641b2
Dhruvin <dhruvinshah2411@gmail.com>
2021-04-12 11:43:08 -0700

    Background-changes

---
+++ Assets/Scripts/OverallBackground.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverallBackground : MonoBehaviour
+{
+    public float bgSpeed;
+    public Renderer bgRend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgRend = GetComponent<Renderer> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bgRend.material.mainTextureOffset += new Vector2(0f,bgSpeed*Time.deltaTime);
+    }
+}
